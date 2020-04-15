ddf36550be4d898209e6559cc07d12a7da57d2a6
jhan1998@gmail.com <jhan1998@gmail.com>
2020-04-15 22:19:20 +0800

    test

--- aoe.cpp
+++ aoe.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv){
 	int matrix[100][100];
 	
 	for(int i = 0 ; i < 100 ; i++){
